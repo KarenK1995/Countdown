9af2ae58b933fb0dc6bedd1a814f9b1610b52ffc
Kuba Szulaczkowski <kuba.szulaczkowski@gmail.com>
2022-12-14 14:57:27 -0500

    Initial Commit

---
+++ FreelanceCountdowns/FreelanceCountdownsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreelanceCountdownsApp.swift
+//  FreelanceCountdowns
+//
+//  Created by Kuba Szulaczkowski on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FreelanceCountdownsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
